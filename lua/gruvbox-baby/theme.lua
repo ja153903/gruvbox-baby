02c62eb4064402656ec3bd318cb08a3d0a07922e
ja153903 <jaimeabbariao@gmail.com>
2023-09-22 18:20:54 -0400

    feat: trying more stuff

--- lua/gruvbox-baby/theme.lua
+++ lua/gruvbox-baby/theme.lua
@@ -435,10 +435,11 @@ function M.setup(config)
     -- NeoTree
     NeoTreeNormal = { fg = c.foreground, bg = c.background_dark },
     NeoTreeNormalNC = { fg = c.foreground, bg = c.background_dark },
+    NeoTreeTitleBar = { fg = c.soft_yellow, bg = c.background_dark },
     NeoTreeFloatNormal = { fg = c.foreground, bg = c.background_dark },
     NeoTreeFloatBorder = { bg = c.background_dark },
     NeoTreeFloatTitle = {
-      bg = c.forest_green,
+      bg = c.milk,
       fg = c.background,
     },
   }
